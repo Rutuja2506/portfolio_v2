1c2b423596e0b2bcaf16c469e51b2b6c20910083
Rutuja2506 <medhekarrutuja@gmail.com>
2023-06-29 23:58:14 -0700

    commiting

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CardComponent from "./card-component";
-import Pro1 from "../assets/img/project-1.jpg";
-import Pro3 from "../assets/img/project-3.jpg";
+import Pro1 from "../assets/img/p1.jpeg";
+import Pro3 from "../assets/img/p2.jpeg";
 import { Fade } from "react-reveal";
 
 const Projects = () => {
@@ -32,7 +32,7 @@ const Projects = () => {
           </div>
           <div style={{ display: "flex", justifyContent: "center" }}>
             <a
-              href="https://github.com/roshan0708"
+              href="https://github.com/Shriraj26"
               target="_blank"
               rel="noopener noreferrer"
               type="button"
@@ -52,36 +52,35 @@ export default Projects;
 
 const ProjectData = [
   {
-    title: "HealthDoc",
-    img: "https://user-images.githubusercontent.com/60403638/118406404-93272600-b699-11eb-8609-3e222af021b1.png",
-    skills: "React.js, Redux, SASS, Node.js, Express.js, MongoDB, and Django",
+    title: "Project Management Tool",
+    img: Pro1,
+    skills: "React, Redux, Next.JS, Chakra UI, Node.JS, Express, Firebase, Socket.IO",
     github: "https://github.com/roshan0708/HealthDoc",
-    text: "Designed and developed an ecommerce site where user can purchase clothes and make payments along with the signin and signup functionality.",
+    text: "Developed a robust project management tool akin to Jira, facilitating the creation of projects and seamless assignment of tasks to users. Furthermore, the system incorporates the ability to attach comments to tasks, promoting effective collaboration.",
     external: "https://healthdoc.herokuapp.com/",
   },
   {
-    title: "MeTalk",
-    img: "https://user-images.githubusercontent.com/60403638/122263235-d58b8f00-cef3-11eb-88ff-a6172ed924b7.png",
-    skills:
-      "CSS, React.js, Redux, Semantic-UI-React, Semantic-UI-CSS, and Firebase",
-    github: "https://github.com/roshan0708/MeTalk",
-    text: "Created a group/one-to-one chat application using Firebase with capabilities of joining “groups” of other hosts.",
-    external: "",
-  },
-  {
-    title: "Ecommerce Site",
-    img: Pro1,
-    skills: "React.js, Redux, Firebase, Stripe API, SASS",
-    github: "https://github.com/roshan0708/React-ecommerce",
-    text: "Designed and developed an ecommerce site where user can purchase clothes and make payments along with the signin and signup functionality.",
-    external: "https://crwn-live0708.herokuapp.com/",
-  },
-  {
-    title: "Recipe App",
+    title: "In-Memory Database",
     img: Pro3,
-    skills: "React.js, Edamam API",
-    github: "https://github.com/roshan0708/recipe_app",
-    text: "Constructed web app for searching recipe of a food item and displaying it in the form of tables.",
-    external: "https://roshan0708.github.io/recipe_app/",
-  },
+    skills: "Java, Design Patterns",
+    github: "https://github.com/roshan0708/MeTalk",
+    text: "Built an in-memory database in Java that stores values of several data types, such as Strings, Arrays, Objects, and Primitives.",
+    external: ""
+  }
+  // {
+  //   title: "Ecommerce Site",
+  //   img: Pro1,
+  //   skills: "React.js, Redux, Firebase, Stripe API, SASS",
+  //   github: "https://github.com/roshan0708/React-ecommerce",
+  //   text: "Designed and developed an ecommerce site where user can purchase clothes and make payments along with the signin and signup functionality.",
+  //   external: "https://crwn-live0708.herokuapp.com/",
+  // },
+  // {
+  //   title: "Recipe App",
+  //   img: Pro3,
+  //   skills: "React.js, Edamam API",
+  //   github: "https://github.com/roshan0708/recipe_app",
+  //   text: "Constructed web app for searching recipe of a food item and displaying it in the form of tables.",
+  //   external: "https://roshan0708.github.io/recipe_app/",
+  // },
 ];
